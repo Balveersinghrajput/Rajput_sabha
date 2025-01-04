2fd88904b758f5695dc9c9bc0904d2701253a466
Balveersingh Rajput <balveersinghrajput2004@gmail.com>
2025-01-04 14:40:24 +0530

    first commit

---
+++ library.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.accordion-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+
+        // Toggle the visibility of the content
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+    });
+});
+
+
+
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+    let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+    for (var  i = currentItem; i < currentItem + 3; i++){
+        boxes[i].style.display = 'inline-block';
+    };
+    currentItem += 3;
+    if(currentItem >= boxes.length){
+        loadMoreBtn.style.display = 'none';
+    }
+}
